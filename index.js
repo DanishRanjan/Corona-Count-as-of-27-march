86fc20081d36fc94dc515172aec86c3bb8d86024
Danish Ranjan <42461549+DanishRanjan@users.noreply.github.com>
2020-06-14 12:19:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+function updateMap() {
+console.log("Updating map with rt data");
+
+    fetch("/data.json")
+        .then(response => response.json())
+        .then(rsp => {
+            // console.log(rsp.data);
+            rsp.data.forEach(element => {
+                latitude = element.latitude;
+                longitude = element.longitude;
+
+                cases = element.infected;
+                if(cases>255){
+                    color = "rgb(255, 0, 0)"
+                }
+                else{
+                    color = `rgb(${cases})`
+                }
+
+                //mark on the map
+                new mapboxgl.Marker({
+                    draggable: false,
+                    color: color
+                    
+                })
+                    .setLngLat([longitude, latitude])
+                    .addTo(map);
+
+
+
+            });
+
+        })
+
+}
+let interval = 2000;
+setInterval(updateMap, interval)
+
+
+
+
+
+//pk.eyJ1IjoiZGFuaXNocmFuamFuOTAxIiwiYSI6ImNrYmVtdDhscDAwNTgzMW12b3g0c3hzMm8ifQ.ge4PVOUpiB3KjojyjX2now
